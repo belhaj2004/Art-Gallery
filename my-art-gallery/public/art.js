8a54f8b21164e8fdd49ed4b1d2210353c50def12
belhaj2004 <belhaj2004@gmail.com>
2024-09-15 15:01:27 -0400

    Changing the styles of text between two divs

---
+++ my-art-gallery/public/art.js
@@ -0,0 +1,25 @@
+// console.log("hello");
+
+// const firstDiv = document.querySelector('.first');
+// const secondDiv = document.querySelector('.second');
+
+// function checkOverlap() {
+//   const firstRect = firstDiv.getBoundingClientRect();
+//   const secondRect = secondDiv.getBoundingClientRect();
+
+//   if (
+//     firstRect.right > secondRect.left && 
+//     firstRect.left < secondRect.right &&
+//     firstRect.bottom > secondRect.top && 
+//     firstRect.top < secondRect.bottom
+//   ) {
+//     firstDiv.style.color = 'black'; // Change color to match .second's color
+//   } else {
+//     firstDiv.style.color = 'white'; // Reset to original color
+//   }
+// }
+
+// // Run the checkOverlap function when the page is loaded and when it is resized
+// window.addEventListener('load', checkOverlap);
+// // window.addEventListener('resize', checkOverlap);
+
